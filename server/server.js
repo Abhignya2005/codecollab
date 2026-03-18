968796686901034cc1ebf79d5bc7ec0e5b61743f
Abhignya2005 <mahankaliabhignya97@gmail.com>
2026-03-18 23:10:16 +0530

    Added signup API with MongoDB integration

--- server/server.js
+++ server/server.js
@@ -1,11 +1,41 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const User = require("./models/user");
 
 const app = express();
 
-// Route
+// Middleware
+app.use(express.json()); // allows JSON data
+app.use(cors()); // allows frontend to connect
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const user = new User({
+      username,
+      email,
+      password
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+mongoose.connect("mongodb://127.0.0.1:27017/codecollab")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Start server
 app.listen(5000, () => {
